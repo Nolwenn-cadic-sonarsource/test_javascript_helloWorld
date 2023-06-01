79598da309bf536bef6d1f00092353fd60951ffa
Nolwenn Cadic <nolwenn.cadic@sonarsource.com>
2023-06-01 14:33:18 +0200

    Add a bug

--- helloWorld.js
+++ helloWorld.js
@@ -1,4 +1,10 @@
 let text = "Hello World";
 let text = "Hello World";
 let text = "Hello";
+let target = -5;
+let num = 3;
+
+target = -num;  // Compliant; intent to assign inverse value of num is clear
+target += num;
+alert("Hello World")
 console.log(text);
