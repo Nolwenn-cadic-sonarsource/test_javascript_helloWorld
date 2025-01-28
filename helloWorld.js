9f7bd4aad95498f05961d097f08df7b00c1aac03
Nolwenn Cadic <nolwenn.cadic@sonarsource.com>
2025-01-28 12:17:32 +0100

    Add another new issue

--- helloWorld.js
+++ helloWorld.js
@@ -1,4 +1,9 @@
-text = "Hello World";
-text = "Hello World";
-text = "Hello World";
+let text = "Hello World";
 console.log(text);
+
+function add(a, b) {
+    let result = a + b; // Noncompliant: Useless assignment to local variable 'result'
+    return a + b;
+}
+
+console.log(add(5, 10));
